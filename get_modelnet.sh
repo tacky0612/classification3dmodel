575109a9a61ccfd684eab91a72e25329c3208a28
tacky0612 <tacky.g25@gmail.com>
2018-10-18 14:56:03 +0900

    add get_modelnet.sh

---
+++ get_modelnet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# download zip files
+wget http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip
+wget http://modelnet.cs.princeton.edu/ModelNet40.zip
+
+# extract tar files
+unzip ModelNet10.zip
+unzip ModelNet40.zip
