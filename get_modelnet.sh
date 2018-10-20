56500be2989996df2853386927a8c1fc4ebd8072
tacky0612 <tacky.g25@gmail.com>
2018-10-21 00:36:04 +0900

    miss typo fixgi

--- get_modelnet.sh
+++ get_modelnet.sh
@@ -5,6 +5,6 @@
 wget http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip
 wget http://modelnet.cs.princeton.edu/ModelNet40.zip
 
-# extract tar files
+# extract zip files
 unzip ModelNet10.zip
 unzip ModelNet40.zip
